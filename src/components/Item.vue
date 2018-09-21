ad0818cd095f522ddae5e4af5487e3055aa8601b
ndero <john.waithaka@andela.com>
2018-09-21 12:45:17 +0300

    ft: add editing item name

--- src/components/Item.vue
+++ src/components/Item.vue
@@ -1,16 +1,55 @@
 <template>
     <li>
-      <span>{{ item.name }}</span>
+      <input type="checkbox" :checked="item.done">
+      <span v-if="!allowEdit">{{ item.name }}</span>
+      <input
+        v-if="allowEdit"
+        v-model="newName"
+        v-on:keyup.enter="editItem"
+        type="text"
+      >
+      <button v-on:click="toggleEdit">Edit</button>
       <button v-on:click="$emit('delete', item.url)">Delete</button>
     </li>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
   props: {
     item: {
       type: Object,
       required: true,
+    },
+  },
+  data() {
+    return {
+      allowEdit: false,
+      newName: this.item.name,
+    };
+  },
+  methods: {
+    toggleEdit() {
+      this.allowEdit = !this.allowEdit;
+    },
+    editItem() {
+      axios({
+        method: 'patch',
+        url: this.item.url,
+        datatype: 'json',
+        headers: {
+          Authorization: `Token ${window.localStorage.getItem('token')}`,
+          'content-Type': 'application/json',
+        },
+        data: {
+          name: this.newName,
+        },
+      })
+        .then(() => {
+          this.item.name = this.newName;
+          this.toggleEdit();
+        });
     },
   },
 };
